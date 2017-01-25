d11e52987b81b936c4236ecfd7fc9bc999d4c394
André Merboldt <andre.merboldt@rwth-aachen.de>
2017-01-25 10:42:31 +0100

    Revert "remove library conflicts with libgit2"
    This reverts commit 7619fee43cd1bd425f990e1ca8fb9bc3d8be566d.

--- sqlite/vector.h
+++ sqlite/vector.h
@@ -28,6 +28,21 @@ void git_vector_free(git_vector *v);
 
 void* git_vector_get(const git_vector *v, size_t position);
 
+inline bool git__multiply_sizet_overflow(size_t *out, size_t one, size_t two)
+{
+  if (one && SIZE_MAX / one < two)
+    return true;
+  *out = one * two;
+  return false;
+}
+
+inline void* git__reallocarray(void *ptr, size_t nelem, size_t elsize)
+{
+  size_t newsize;
+  return git__multiply_sizet_overflow(&newsize, nelem, elsize) ?
+    NULL : realloc(ptr, newsize);
+}
+
 #define git_vector_set_sorted(V,S) do { \
   (V)->flags = (S) ? ((V)->flags | GIT_VECTOR_SORTED) : \
   ((V)->flags & ~GIT_VECTOR_SORTED); } while (0)
