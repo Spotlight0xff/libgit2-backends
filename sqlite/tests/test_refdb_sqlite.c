add191ce36e61b5824eec897390a272d31d013bd
André Merboldt <andre.merboldt@rwth-aachen.de>
2017-01-25 10:42:17 +0100

    c++ -> c for test

--- sqlite/tests/test_refdb_sqlite.c
+++ sqlite/tests/test_refdb_sqlite.c
@@ -10,17 +10,17 @@
 int main(int argc, char** argv) {
   git_refdb_backend* ref_backend = NULL;
   git_refdb* ref_db = NULL;
-  std::string path = "test_repo";
-  std::string file = "test_repo.db";
+  const char* path = "test_repo";
+  const char* file = "test_repo.db";
   int result = 0;
   int error = 0;
   git_repository* m_repo = NULL;
 
   // create fixtur
   git_libgit2_init();
-  result = git_repository_init(&m_repo, path.c_str(), false);
+  result = git_repository_init(&m_repo, path, 0 /* no bare */);
   result = git_refdb_new(&ref_db, m_repo);
-  result = git_refdb_backend_sqlite(&ref_backend, m_repo, file.c_str());
+  result = git_refdb_backend_sqlite(&ref_backend, m_repo, file);
   git_repository_set_refdb(m_repo, ref_db);
 
 
@@ -33,7 +33,7 @@ git_oid_fromstrp(&test_oid0, "hello world!");
 error = git_reference_create(&ref, m_repo,
       "refs/heads/direct",       /* name */
       &test_oid0,                      /* target */
-      true,                      /* force? */
+      1,                      /* force? */
       NULL);                     /* the message for the reflog */
 
 
